13b7366be4d67a1483623774e7208e7f1c6d221b
konecnyna <konecnyna@gmail.com>
2015-05-20 19:00:48 -0400

    added back start kiosk

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var weather = require('./weather.js');
 var mta = require('./mta.js');
 var kodi = require('./kodi.js');
 var app = express();
+var express = require('express');
+var chromeKiosk = '/usr/bin/chromium --kiosk --ignore-certificate-errors --disable-restore-session-state "http://localh
  
 app.get('/api/v1/weather', function(req, res) {    
  weather.getToolbarWeather(function(callback){
@@ -24,3 +26,6 @@ app.get('/api/v1/kodi', function(req, res) {
 
 app.use(express.static(__dirname + '/html'));
 app.listen(8080);
+
+//Start chrome!
+exec(chromeKiosk, function(error,stdout,stderr){});
