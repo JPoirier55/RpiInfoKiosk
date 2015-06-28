cc334ce98095789f915a87a6538ebd14072cfd3d
Nick <konecnyna@gmail.com>
2015-06-28 13:13:13 -0400

    temp

--- python/motion.py
+++ python/motion.py
@@ -19,10 +19,27 @@ def MOTION(PIR_PIN):
 	print "Motion Detected! setting time."
     	global LAST_MOTION_DETECTED
 	LAST_MOTION_DETECTED = datetime.now()
+	
+
+def monitorOff():
+	print "turning monitor off"
+	global MONITOR_STATE 
+	MONITOR_STATE = 0
+	subprocess.call(['tvservice','-o'])
+
+def monitorOn():
 	print "Monitor on"
 	global MONITOR_STATE
 	MONITOR_STATE = 1
-	subprocess.check_call(['/home/pi/Github/RpiInfoKiosk/python/monitor_on.sh'])	
+	#subprocess.check_call(['/home/pi/Github/RpiInfoKiosk/python/monitor_on.sh'])	
+	status = subprocess.check_output(['tvservice', '-s'])
+	if "HDMI" in status:
+		subprocess.check_output(['tvservice', '--explicit="DMT 87"'])
+	    subprocess.check_output(['fbset', '-depth 8'])
+	    subprocess.check_output(['fbset', '-depth 16'])
+	    subprocess.check_output(['xrefresh'])
+	   
+		   
 	
 
 def checkMotion():
@@ -30,10 +47,7 @@ def checkMotion():
 	d = datetime.now() - LAST_MOTION_DETECTED
 	print "Seconds: " + str(d.seconds)
 	if(d.seconds > 30):
-		print "turning monitor off"
-		global MONITOR_STATE 
-		MONITOR_STATE = 0
-		subprocess.call(['tvservice','-o'])
+		monitorOff()
 
 #Main
 print "PIR Module Test (CTRL+C to exit)"
