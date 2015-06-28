417b0871f41396267fc65722ddf8e060ccf3f7ec
Nick <konecnyna@gmail.com>
2015-06-28 14:03:01 -0400

    added logs

--- autostart/start.sh
+++ autostart/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #Run this is @sh <path_to_this> in /home/pi/.config/lxsession/LXDE-pi/autostart 
-nodemon /home/pi/Github/RpiInfoKiosk/server.js &
-sudo python /home/pi/Github/RpiInfoKiosk/python/motion.py &
+nodemon /home/pi/Github/RpiInfoKiosk/server.js >> /home/pi/logs/node.log &
+sudo python /home/pi/Github/RpiInfoKiosk/python/motion.py /home/pi/logs/motion.log &
