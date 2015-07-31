3765d404f12506c8aac09ed76e8cd4ce10f50850
konecnyna <konecnyna@gmail.com>
2015-07-31 16:17:59 -0400

    Update start.sh

--- autostart/start.sh
+++ autostart/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ ! -d /home/pi/logs ]; then
+	mkdir /home/pi/logs
+fi;
+
+if [ ! -d /home/pi/logs/node.log ]; then
+	touch /home/pi/logs/node.log
+fi;
+
+
+
 #Run this is @sh <path_to_this> in /home/pi/.config/lxsession/LXDE-pi/autostart 
 forever start /home/pi/Github/RpiInfoKiosk/server.js >> /home/pi/logs/node.log &
-sudo python /home/pi/Github/RpiInfoKiosk/python/motion.py /home/pi/logs/motion.log &
+sudo python /home/pi/Github/RpiInfoKiosk/python/motion.py >> /home/pi/logs/motion.log &
