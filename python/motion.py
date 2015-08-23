da81b15a9c5523185a4dcc15312ca28c9d69ac3f
Nick Konecny <nick.konecny@dowjones.com>
2015-08-23 11:31:55 -0400

    More python fixes

--- python/motion.py
+++ python/motion.py
@@ -26,7 +26,7 @@ def MOTION(PIR_PIN):
 	
 
 def monitorOn():
-	monitorStatus = subprocess.check_output("tvservice", "-s")
+	monitorStatus = subprocess.check_output("tvservice -s", stderr=STDOUT, shell=True)
 	print monitorStatus
 	if "off" in monitorStatus:
 		subprocess.call("tvservice -p | xset dpms force on | xset -dpms | xset s off | xset s noblank", shell=True)
