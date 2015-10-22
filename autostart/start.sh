cd3f059728e37b22c923aeb5939b38c41e936d5e
Nick Konecny <nick.konecny@dowjones.com>
2015-10-22 18:22:05 -0400

    Revert

--- autostart/start.sh
+++ autostart/start.sh
@@ -8,9 +8,6 @@ if [ ! -d /home/pi/logs/node.log ]; then
 	touch /home/pi/logs/node.log
 fi;
 
-
-
 #Run this is @sh <path_to_this> in /home/pi/.config/lxsession/LXDE-pi/autostart 
-#forever start /home/pi/Github/RpiInfoKiosk/server.js > /home/pi/logs/node.log &
-sudo start kiosk
-sudo python /home/pi/Github/RpiInfoKiosk/python/motion.py > /home/pi/logs/motion.log &
+forever start /home/pi/Github/RpiInfoKiosk/server.js > /home/pi/logs/node.log &
+
