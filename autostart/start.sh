49ca4b2f059fa4cfe9dd5b103a5096e90891c1dd
nkonecny <nkonecny@etsy.com>
2015-10-22 16:46:33 -0400

    Made tit it a service instead

--- autostart/start.sh
+++ autostart/start.sh
@@ -11,5 +11,6 @@ fi;
 
 
 #Run this is @sh <path_to_this> in /home/pi/.config/lxsession/LXDE-pi/autostart 
-forever start /home/pi/Github/RpiInfoKiosk/server.js > /home/pi/logs/node.log &
+#forever start /home/pi/Github/RpiInfoKiosk/server.js > /home/pi/logs/node.log &
+sudo start autostart
 sudo python /home/pi/Github/RpiInfoKiosk/python/motion.py > /home/pi/logs/motion.log &
