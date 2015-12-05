d7a963afed17d1858bbc0f5e5b0d8fe6da318f7d
Nick Konecny <nick.konecny@dowjones.com>
2015-12-05 18:30:36 -0500

    ugh

--- autostart/start.sh
+++ autostart/start.sh
@@ -10,5 +10,5 @@ fi;
 
 #Run this is @sh <path_to_this> in /home/pi/.config/lxsession/LXDE-pi/autostart 
 forever start /home/pi/Github/RpiInfoKiosk/server.js > /home/pi/logs/node.log &
-sudo python /home/pi/Github/RpiInfoKiosk/python/motion.py >> /home/pi/logs/motion.log &
+sudo python /home/pi/Github/RpiInfoKiosk/python/motion.py > /home/pi/logs/motion.log 2>&1 &
 
