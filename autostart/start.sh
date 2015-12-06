c68960dc82fc0bd16789ed2045e30c3750e8ac7f
Nick Konecny <nick.konecny@dowjones.com>
2015-12-06 13:18:10 -0500

    Debugggin

--- autostart/start.sh
+++ autostart/start.sh
@@ -9,6 +9,7 @@
 # fi;
 
 #Run this is @sh <path_to_this> in /home/pi/.config/lxsession/LXDE-pi/autostart 
-forever start /home/pi/Github/RpiInfoKiosk/server.js > /home/pi/logs/node.log &
+echo "Begin script" >> /home/pi/logs/debug.log
 sudo python /home/pi/Github/RpiInfoKiosk/python/motion.py > /home/pi/logs/motion.log &
-
+forever start /home/pi/Github/RpiInfoKiosk/server.js > /home/pi/logs/node.log &
+echo "End script" >> /home/pi/logs/debug.log
