89f0579e85c3145a26f6713a282e4e82b6e24696
Nick Konecny <nkonecny@etsy.com>
2015-12-09 17:37:38 -0500

    yup

--- autostart/start.sh
+++ autostart/start.sh
@@ -10,5 +10,5 @@
 
 #Run this is @sh <path_to_this> in /home/pi/.config/lxsession/LXDE-pi/autostart 
 forever start /home/pi/Github/RpiInfoKiosk/server.js > /home/pi/logs/node.log 2>&1 &
-sudo python /home/pi/Github/RpiInfoKiosk/python/motion.py > /home/pi/logs/motion.log 2>&1 &
+sudo python /home/pi/Github/RpiInfoKiosk/python/motion.py
 
