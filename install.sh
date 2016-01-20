032e1a810fb7c28c559a2fcc2a9b92b1bfcca065
Kiosk <konecnyna@gmail.com>
2016-01-19 21:23:36 -0500

    Add init scipt

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+	
+#sudo apt-get update
+#sudo apt-get upgrade
+#echo "Updated, installing depnecies"
+
+#sudo apt-get install chromium-browser x11-xserver-utils upstart xdotool
+
+
+"Installing node..."
+
+wget https://nodejs.org/download/release/v0.10.0/node-v0.10.0-linux-arm-pi.tar.gz
+
+pwdir=$(pwd)
+
+cd /usr/local
+sudo tar xzvf $pwdir/node-v0.10.0-linux-arm-pi.tar.gz --strip=1
+rm $pwdir/node-v0.10.0-linux-arm-pi.tar.gz
+cd pwdir
+
+
+"Updating node..."
+sudo npm install -g n
+sudo n stable
+
+
+sudo npm install forever -g
+sudo npm install
+
+
+"Linking upstart file to start kiosk on boot..."
+mkdir -p /home/pi/.config/lxsession/LXDE-pi/
+
+ln autostart/autostart /home/pi/.config/lxsession/LXDE-pi/
+
+
+echo "Donezo!"
+
