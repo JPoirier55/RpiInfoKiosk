41361772eaa683a07a341b90ea0bc4c6aa899ecf
Nick Konecny <nick.konecny@dowjones.com>
2016-01-25 20:57:22 -0500

    revert

--- autostart/start.sh
+++ autostart/start.sh
@@ -9,6 +9,6 @@
 # fi;
 
 #Run this is @sh <path_to_this> in /home/pi/.config/lxsession/LXDE-pi/autostart 
-forever -l /home/pi/logs/node.log start /home/pi/Github/RpiInfoKiosk/server.js 
+forever start /home/pi/Github/RpiInfoKiosk/server.js 
 sudo python /home/pi/Github/RpiInfoKiosk/python/motion.py
 
