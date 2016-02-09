44cc3e5c9ab643f9a1eff474a487b4b6911e7237
Nick Konecny <nick.konecny@dowjones.com>
2016-02-08 20:57:56 -0500

    Fixed calendar

--- calendar.js
+++ calendar.js
@@ -31,16 +31,16 @@ function getCalendarJson(callback){
 		var days = calendar.indexOf("konecny") ? daysOut : 3;
 		endDateISO.setDate(endDateISO.getDate() + days);
 
-		var url = util.format("https://www.googleapis.com/calendar/v3/calendars/%s/events?key=%s&timeMin=%s&timeMax=%s&singleEvents=%s&orderBy=%s",calendar, calApiKey,dateISO,endDateISO.toISOString(),"true","startTime");	
+		var url = util.format("https://www.googleapis.com/calendar/v3/calendars/%s/events?key=%s&timeMin=%s&timeMax=%s&singleEvents=%s&orderBy=%s",calendar, calApiKey,dateISO,endDateISO.toISOString(),"true","startTime");
 		utils.downloadFileSSL(url, function(json){
 			json = JSON.parse(json);
-			if(json.items.legnth > 0){
+			if(json.items.length > 0){
 				calendarsJSON.push(json.items[0]);
 			}
-			callback();		
+			callback();
 		});
-		
-	}, function(err){				
+
+	}, function(err){
 		addExtras(getClosestEvent(calendarsJSON), callback);
 	});
 }
@@ -61,7 +61,7 @@ function getClosestEvent(calendars){
 		return;
 	}
 
-	
+
 	var date1 = new Date(calendars[0].start.date);
 	var date2 = new Date(calendars[1].start.date);
 	if(date1 < date2){
@@ -76,7 +76,7 @@ function addExtras(calendarEvent,callback){
 		var now = moment();
 		var date1;
 		if(calendarEvent.creator.email === "usa__en@holiday.calendar.google.com"){
-			date1 = new moment(calendarEvent.start.date);			
+			date1 = new moment(calendarEvent.start.date);
 		}else{
  			date1 = new moment(calendarEvent.start.dateTime);
 		}
@@ -87,55 +87,55 @@ function addExtras(calendarEvent,callback){
 			calendarEvent.background = "#FF9800";
 			calendarEvent.backgroundImage ="";
 			calendarEvent.textColor = "#000";
-			calendarEvent.dateColor = "#FFEB3B";	
+			calendarEvent.dateColor = "#FFEB3B";
 
 		}else if(currentMonth === 11){
 			calendarEvent.icon = "http://img.brothersoft.com/games/flash/icon/t/thanksgiving-pop-720p-92610-1325408900.png";
 			calendarEvent.background = "#795548";
 			calendarEvent.backgroundImage ="";
 			calendarEvent.textColor = "#FDD835";
-			calendarEvent.dateColor = "#FF9800";	
+			calendarEvent.dateColor = "#FF9800";
 
 		}else if(currentMonth === 12){
 			calendarEvent.icon = "http://png-5.findicons.com/files/icons/243/winter_holiday/128/xmas_tree.png";
 			calendarEvent.background = "#8BC34A";
 			calendarEvent.backgroundImage ="";
 			calendarEvent.textColor = "#F44336";
-			calendarEvent.dateColor = "#F44336";		
+			calendarEvent.dateColor = "#F44336";
 
 		}else{
 			calendarEvent.icon = "http://uwahockey.org.au/wp-content/uploads/2015/02/news-icon-100x100.png";
 			calendarEvent.background = "#4CAF50";
 			calendarEvent.backgroundImage ="";
 			calendarEvent.textColor = "#FFFF8D";
-			calendarEvent.dateColor = "#FFEB3B";				
+			calendarEvent.dateColor = "#FFEB3B";
 		}
 
-		var diff = date1.diff(now, 'days');	
+		var diff = date1.diff(now, 'days');
 		var diffHours = date1.diff(now, 'hours');
 		var isHolidayCal = calendarEvent.creator.email === "usa__en@holiday.calendar.google.com";
 
 		if(diff > 2 && diff <= 7){
-			calendarEvent.start.date = "in " + (date1.diff(now, 'days') + 1) + " days.";		
+			calendarEvent.start.date = "in " + (date1.diff(now, 'days') + 1) + " days.";
 		}else if(diff === 0){
 			if(diffHours <= 12){
 				if(isHolidayCal){
 					calendarEvent.summary = calendarEvent.summary.replace('(regional holiday)','');
 					calendarEvent.start.date = "Today!";
 				}else{
-					calendarEvent.start.date = "Today! at: " + date1.format('hh:mm a');			
+					calendarEvent.start.date = "Today! at: " + date1.format('hh:mm a');
 				}
 			}else{
 				calendarEvent.start.date = "Tommorrow";
-			}			
+			}
 		}else{
-			calendarEvent.start.date = "on " + date1.format("ddd, MMM DD") + ".";						
+			calendarEvent.start.date = "on " + date1.format("ddd, MMM DD") + ".";
 		}
 
 		if(calendarEvent.location){
 			var loc = calendarEvent.location.replace(/, /g, '');
 			loc = loc.replace(/ /g, '+');
-			calendarEvent.map_src = util.format(mapSrc, mapsApiKey, loc);	
+			calendarEvent.map_src = util.format(mapSrc, mapsApiKey, loc);
 		}
 
 		if(isHolidayCal){
@@ -145,27 +145,27 @@ function addExtras(calendarEvent,callback){
 		}else{
 			callback(calendarEvent);
 		}
-		
+
 	}else{
 		callback(calendarEvent);
 	}
-	
+
 
 }
 
 function getGoolgeImageResult(calendarEvent, callback){
 	var googleImgApiUrl = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s%s";
-	googleImgApiUrl = util.format(googleImgApiUrl, calendarEvent.summary, " holiday image");	
+	googleImgApiUrl = util.format(googleImgApiUrl, calendarEvent.summary, " holiday image");
 	utils.downloadFileSSL(googleImgApiUrl, function(json){
 		json = JSON.parse(json);
 		if(json.responseData){
 			var randomIndex = Math.floor((Math.random() * json.responseData.results.length));
 			if(randomIndex){
-				calendarEvent.img_url = json.responseData.results[randomIndex].url;			
+				calendarEvent.img_url = json.responseData.results[randomIndex].url;
 			}else{
 				calendarEvent.img_url = json.responseData.results[0].url;
 			}
 		}
-		callback();		
+		callback();
 	});
 }
